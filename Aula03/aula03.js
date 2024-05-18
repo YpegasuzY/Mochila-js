23e5f309a9b298c4359a26e62e1966c51c226be2
Raphael Silva <Raphael010601@gmail.com>
2024-05-17 21:02:16 -0300

    update

--- Aula03/aula03.js
+++ Aula03/aula03.js
@@ -1,15 +1,15 @@
-console.log('Minha primeira Mensagem');
+// console.log('Minha primeira Mensagem');
 
 const mensagemTela = ('Texto 01');
 console.log(mensagemTela);
 
-mensagemTela = 'Alterando informações'
-console.log(mensagemTela);
+//mensagemTela = 'Alterando informações'
+//console.log(mensagemTela);
 
-let mensagemExemplo = ('Esta mensagem é um teste');
-console.log(mensagemExemplo);
+//let mensagemExemplo = ('Esta mensagem é um teste');
+//console.log(mensagemExemplo);
 
-mensagemExemplo = 'Alteração textual'
-console.log(mensagemExemplo);
+//mensagemExemplo = 'Alteração textual'
+//console.log(mensagemExemplo);
 
 //node + nome da pasta + nome do arquivo; para fazer rodar no js
