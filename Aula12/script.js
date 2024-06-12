e990158cfd81ed3506f434e85ff6cf89ca380248
Raphael Silva <Raphael010601@gmail.com>
2024-06-12 19:51:28 -0300

    Update

--- Aula12/script.js
+++ Aula12/script.js
@@ -1,15 +1,20 @@
 let meuElemento = document.getElementById('meuElemento');
 
 function tornarCirculo() {
-    meuElemento.classList.add('circle');
-    meuElemento.classList.remove('square');
+    meuElemento.classList.add('circle'); //adicionar a classe circle
+    meuElemento.classList.remove('square'); //remover a classe square
 }
 
 function tornarSquare() {
-    meuElemento.classList.add('square');
-    meuElemento.classList.remove('circle')
+    meuElemento.classList.add('square'); //adicionar a classe circle
+    meuElemento.classList.remove('circle') //remover a classe square
 }
 
-function alternar(){
+function alternarFormato(){
+    meuElemento.classList.toggle('circle')
+    meuElemento.classList.toggle('square')
+}
 
+function VerificarFormato(){
+    
 }
