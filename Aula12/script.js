120080e2239feb674a79e137ed4d9d11f13c9bbe
Raphael Silva <Raphael010601@gmail.com>
2024-06-11 21:41:16 -0300

    update

---
+++ Aula12/script.js
@@ -0,0 +1,15 @@
+let meuElemento = document.getElementById('meuElemento');
+
+function tornarCirculo() {
+    meuElemento.classList.add('circle');
+    meuElemento.classList.remove('square');
+}
+
+function tornarSquare() {
+    meuElemento.classList.add('square');
+    meuElemento.classList.remove('circle')
+}
+
+function alternar(){
+
+}
