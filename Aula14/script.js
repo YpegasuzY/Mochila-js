7ba99be6706a363f079baadb7834cedafdf5a16b
Raphael Silva <Raphael010601@gmail.com>
2024-06-18 20:32:19 -0300

    update

--- Aula14/script.js
+++ Aula14/script.js
@@ -1,27 +1,18 @@
-const input = document.getElementById('taskInput'); //input
+const hamburgerButton = document.querySelector("#hamburgerButton"); //Estamos recuperando o Botão de hamburguer (Fora da Div).
+const closeButton = document.querySelector("#closeButton");
+//Estamos recuperando o Botão de Fechamento.
 
-const ul = document.getElementById('taskList'); //lista
+const mobileMenu = document.querySelector("#mobileMenu");
+// Estamos recuperando a caixa preta (Quando aberta o menu Hamburguer).
 
-function addTask() {//Aqui criamos uma função para ser atribuída dentro do botão
-    if(input.value !== "") { //Aqui informamos que se (if) o input for diferente (!==) de vazio (""), ele irá criar um elemento (createElement) de "li", o texto que ela possui é igual o "input value", ou seja, é o valor do input que será digitado.
-        const li = document.createElement("li");
-        li.textContent = input.value;
-        //Traduzindo esta linha, é o texto de lista (li)que será criado, deverá ser igual o valor que será digitado dentro do campo "input"
-        const removerBotao = document.createElement("button"); //Aqui estasmos criando um botão (button) para remover esses itens de (li);
+//Aqui adicionamos um evento de escutar o click do Mouse sobre o ícone de Hamburguer.
+hamburgerButton.addEventListener("click", function() {
+    mobileMenu.classList.add("flex");
+    //criamos uma função que a ouvir o click do Mouse, ele adicione uma Class, que está fazendo nossa caixa (DIV) aparecer, ou seja, ficar visível.
+});
 
-        removerBotao.textContent = "remover"; //Chamamos nossa variável "removerBotao" e inserindo nele um texto chamado "Remover".
 
-        removerBotao.onclick = function() { //Criamos uma Função que ao clicar neste botão ele irá executar uma função.
-            ul.removeChild(li); //A função é pegar o elemento Filho (child) do UL, que no caso é a "li" e remove-la.
-        };
-
-        ul.appendChild(li); //Estamos adicionando um filho para nossa UL.
-        li.appendChild(removerBotao); //Aquichamamos nossa const "li" e estamos adicionando a ela um filho (appendChild)
-
-        input.value = ""; // Usado para limpar o campo Input, depois de adicionado o item em questão.
-    } else { //Caso o campo input estiver vazio, ele irá aparecer esta mensagem.
-        alert("Por favor, insira uma tarefa");
-
-    }
-
-}
+//criamos uma função que a ouvir o click do Mouse, ele REMOVA uma Class, que está fazendo nossa caixa (DIV) aparecer, ou seja, ficar visível.
+closeButton.addEventListener("click", function() {
+    mobileMenu.classList.remove("flex");
+});
